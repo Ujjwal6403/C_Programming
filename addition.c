446c837c4970863ba93730177eed400a7ae12ec4
ujjwalnarkhede <ujjwalnarkhede2003@gmail.com>
2023-04-02 18:35:38 +0530

    addition of two numbers

---
+++ addition.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int number1, number2, number3;
+    printf("enter the number \n");
+    scanf("%d%d",&number1,&number2);scanf("%d%d",&number1,&number2);
+    scanf("%d%d",&number1,&number2);scanf("%d%d",&number1,&number2);
+
+    number3= number1 % number2;
+    number3= number1 * number2;
+    number3= number1 + number2;
+    number3= number1 - number2;
+    
+    printf("the sum of %d",number3,number3,number3);
+        return 0;
+
+}
